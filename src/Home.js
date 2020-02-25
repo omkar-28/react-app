1a6f040dae9831b42a9c8f4a4c5bb5ea46a6a807
omkar <omikhatavkar2@gmail.com>
2020-02-25 11:18:24 +0530

    First react app

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Hellow Home Page</h2>
+                <p>ShoppingList is a React component class, or React component type. A component takes in parameters, called props (short for “properties”), and returns a hierarchy of views to display via the render method.</p>
+            </div>
+        )
+    }
+}
+
+export default Home;
